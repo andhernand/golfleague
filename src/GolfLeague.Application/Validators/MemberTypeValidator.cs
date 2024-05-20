266a2c1e9614e8b472e615d286d3e377e1135df7
Andres Hernandez <andhernand@gmail.com>
2024-05-20 16:26:15 -0600

    Altered the member type validation
    the database existence check will only run when the name is not empty.

--- src/GolfLeague.Application/Validators/MemberTypeValidator.cs
+++ src/GolfLeague.Application/Validators/MemberTypeValidator.cs
@@ -8,18 +8,20 @@ namespace GolfLeague.Application.Validators;
 public class MemberTypeValidator : AbstractValidator<MemberType>
 {
     private readonly IMemberTypeRepository _memberTypeRepository;
+
     public MemberTypeValidator(IMemberTypeRepository memberTypeRepository)
     {
         _memberTypeRepository = memberTypeRepository;
-        
+
         RuleFor(mt => mt.Name)
-            .NotEmpty();
-        
-        RuleFor(x => x.Name)
-            .MustAsync(ValidateName)
-            .WithMessage("This Member Type already exists in the system");
+            .NotEmpty()
+            .DependentRules(() =>
+                RuleFor(mt => mt.Name)
+                    .MustAsync(ValidateName)
+                    .WithMessage("This Member Type already exists in the system")
+            );
     }
-    
+
     private async Task<bool> ValidateName(MemberType memberType, string name, CancellationToken token = default)
     {
         var existingMemberType = await _memberTypeRepository.GetMemberTypeByNameAsync(name, token);
