93d8adb3a0df1af75854eebba1d0ac12de5da560
Andres Hernandez <andhernand@gmail.com>
2024-05-26 20:16:27 -0600

    small changes to the docker compose file
    added else statements to notify that the database or login already
    exists.

--- db/init.sql
+++ db/init.sql
@@ -1,16 +1,21 @@
 IF (DB_ID(N'GolfLeague') IS NULL)
-BEGIN
-	CREATE DATABASE [GolfLeague];
-END;
+	BEGIN
+		PRINT 'Creating the database.';
+		CREATE DATABASE [GolfLeague];
+	END
+ELSE
+	BEGIN
+		PRINT 'Database already exists.';
+	END;
 GO
 
-IF NOT EXISTS
-(
-	SELECT 1
-	FROM [master].[sys].[server_principals]
-	WHERE [name] = N'golfleagueapi' and [type] IN ('C','E', 'G', 'K', 'S', 'U')
-)
-BEGIN
-	CREATE LOGIN [golfleagueapi] WITH PASSWORD=N'$(varApiPassword)', DEFAULT_DATABASE=[GolfLeague];
-END;
+IF NOT EXISTS(SELECT 1 FROM [master].[sys].[server_principals] WHERE [name] = N'golfleagueapi' AND [type] IN ('C', 'E', 'G', 'K', 'S', 'U'))
+	BEGIN
+		PRINT 'Creating the login.';
+		CREATE LOGIN [golfleagueapi] WITH PASSWORD =N'$(varApiPassword)', DEFAULT_DATABASE = [GolfLeague];
+	END
+ELSE
+	BEGIN
+		PRINT 'Login already exists.';
+	END;
 GO
