5f3dac501acb34bf463d8f57e63fa862b50bf639
Andres Hernandez <andhernand@gmail.com>
2024-06-20 20:36:06 -0600

    beginning work on UI
    Added SSL to the web ui. start web ui when api starts.
    
    added prettier step to CI.  get the project ready to start coding UI.

--- src/golfleague.web/vite.config.js
+++ src/golfleague.web/vite.config.js
@@ -1,7 +1,59 @@
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
+import child_process from 'child_process';
+import { env } from 'process';
 
-// https://vitejs.dev/config/
+const customPort = 9988;
+const certificateName = 'golfleague.web';
+const certFilePath = path.join('./', `${certificateName}.pem`);
+const keyFilePath = path.join('./', `${certificateName}.key`);
+
+if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+  if (0 !== child_process.spawnSync('dotnet', [
+    'dev-certs',
+    'https',
+    '--export-path',
+    certFilePath,
+    '--format',
+    'Pem',
+    '--no-password'
+  ], { stdio: 'inherit' }).status) {
+    throw new Error('Could not create certificate.');
+  }
+}
+
+const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7184';
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      '^/api/golfers': {
+        target,
+        secure: false
+      },
+      '^/api/tournaments': {
+        target,
+        secure: false
+      },
+      '^/api/tournamentparticipation': {
+        target,
+        secure: false
+      }
+    },
+    port: customPort,
+    https: {
+      key: fs.readFileSync(keyFilePath),
+      cert: fs.readFileSync(certFilePath)
+    }
+  }
 });
