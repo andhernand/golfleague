f2530da4652e760a28e2d7f8e5b1bedc231a7bc2
Andres Hernandez <andhernand@gmail.com>
2024-09-09 16:17:40 -0600

    Alter the way Serilog is bootstrapped and used.
    also added dependencies and updated the way services are injected.

--- src/GolfLeague.Api/Program.cs
+++ src/GolfLeague.Api/Program.cs
@@ -3,6 +3,7 @@ using System.Text;
 using GolfLeague.Api.Auth;
 using GolfLeague.Api.Endpoints;
 using GolfLeague.Api.Health;
+using GolfLeague.Api.Logging;
 using GolfLeague.Api.Mapping;
 using GolfLeague.Api.Options;
 using GolfLeague.Api.Swagger;
@@ -16,69 +17,82 @@ using Serilog;
 
 using Swashbuckle.AspNetCore.SwaggerGen;
 
-var builder = WebApplication.CreateBuilder(args);
-using var config = builder.Configuration;
+Log.Logger = ApiLogging.CreateBootstrapLogger();
 
-Log.Logger = new LoggerConfiguration()
-    .ReadFrom.Configuration(config)
-    .CreateLogger();
-
-builder.Host.UseSerilog();
-
-var jwtOptions = new JwtOptions();
-config.GetSection("JwtSettings").Bind(jwtOptions);
-
-builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-    })
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters.IssuerSigningKey = new SymmetricSecurityKey(
-            Encoding.UTF8.GetBytes(jwtOptions.Key));
-        options.TokenValidationParameters.ValidIssuer = jwtOptions.Issuer;
-        options.TokenValidationParameters.ValidAudience = jwtOptions.Audience;
-        options.TokenValidationParameters.ClockSkew = TimeSpan.FromSeconds(5);
-        options.TokenValidationParameters.ValidateIssuerSigningKey = true;
-        options.TokenValidationParameters.ValidateLifetime = true;
-        options.TokenValidationParameters.ValidateIssuer = true;
-        options.TokenValidationParameters.ValidateAudience = true;
-    });
-
-builder.Services.AddAuthorizationBuilder()
-    .AddPolicy(AuthConstants.AdminPolicyName, p => p.RequireClaim(AuthConstants.AdminClaimName, "true"))
-    .AddPolicy(AuthConstants.TrustedPolicyName, p => p.RequireAssertion(c =>
-        c.User.HasClaim(m => m is { Type: AuthConstants.AdminClaimName, Value: "true" })
-        || c.User.HasClaim(m => m is { Type: AuthConstants.TrustedMemberClaimName, Value: "true" })));
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
-
-builder.Services
-    .AddHealthChecks()
-    .AddCheck<DatabaseHealthCheck>(DatabaseHealthCheck.Name);
-
-builder.Services.AddGolfLeagueApplication();
-builder.Services.AddGolfLeagueDatabase(config["Database:ConnectionString"]!);
-
-await using var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+    {
+        builder.Host.UseSerilog((context, logConfig) =>
+            logConfig.ReadFrom.Configuration(context.Configuration));
+
+        using var config = builder.Configuration;
+
+        var jwtOptions = new JwtOptions();
+        config.GetSection("JwtSettings").Bind(jwtOptions);
+
+        builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters.IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(jwtOptions.Key));
+                options.TokenValidationParameters.ValidIssuer = jwtOptions.Issuer;
+                options.TokenValidationParameters.ValidAudience = jwtOptions.Audience;
+                options.TokenValidationParameters.ClockSkew = TimeSpan.FromSeconds(5);
+                options.TokenValidationParameters.ValidateIssuerSigningKey = true;
+                options.TokenValidationParameters.ValidateLifetime = true;
+                options.TokenValidationParameters.ValidateIssuer = true;
+                options.TokenValidationParameters.ValidateAudience = true;
+            });
+
+        builder.Services.AddAuthorizationBuilder()
+            .AddPolicy(AuthConstants.AdminPolicyName, p => p.RequireClaim(AuthConstants.AdminClaimName, "true"))
+            .AddPolicy(AuthConstants.TrustedPolicyName, p => p.RequireAssertion(c =>
+                c.User.HasClaim(m => m is { Type: AuthConstants.AdminClaimName, Value: "true" })
+                || c.User.HasClaim(m => m is { Type: AuthConstants.TrustedMemberClaimName, Value: "true" })));
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+
+        builder.Services
+            .AddHealthChecks()
+            .AddCheck<DatabaseHealthCheck>(DatabaseHealthCheck.Name);
+
+        builder.Services.AddGolfLeagueApplication();
+        builder.Services.AddGolfLeagueDatabase(config["Database:ConnectionString"]!);
+    }
+
+    await using var app = builder.Build();
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseHealthChecks(new PathString("/_health"));
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseMiddleware<ValidationMappingMiddleware>();
+        app.MapApiEndpoints();
+    }
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseHealthChecks(new PathString("/_health"));
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.UseMiddleware<ValidationMappingMiddleware>();
-app.MapApiEndpoints();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
