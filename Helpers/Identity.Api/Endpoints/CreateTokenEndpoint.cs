5c850170f888c4a8347d8e36925cf3ce8dc48202
Andres Hernandez <andhernand@gmail.com>
2024-09-09 17:03:05 -0600

    Change the way serilog is bootstrapped and used.

--- Helpers/Identity.Api/Endpoints/CreateTokenEndpoint.cs
+++ Helpers/Identity.Api/Endpoints/CreateTokenEndpoint.cs
@@ -7,20 +7,19 @@ public static class CreateTokenEndpoint
     private const string Name = "CreateToken";
     private static readonly Serilog.ILogger Logger = Log.ForContext(typeof(CreateTokenEndpoint));
 
-    public static IEndpointRouteBuilder MapCreateToken(this IEndpointRouteBuilder app)
+    public static void MapCreateToken(this IEndpointRouteBuilder app)
     {
         app.MapPost(IdentityApiConstants.Tokens.Create, (TokenGenerationRequest request) =>
             {
-                Logger.Information("Generating {@TokenGenerationRequest}", request);
+                Logger.Information("Creating a JWT with the following Claims: {@TokenGenerationRequest}", request);
+
                 var jwt = JwtGenerator.GenerateToken(request.IsAdmin, request.IsTrusted);
-                Logger.Verbose("Created: {JwtToken}", jwt);
                 return TypedResults.Text(jwt);
             })
             .WithName(Name)
             .WithTags(IdentityApiConstants.Tokens.Tag)
-            .Produces<string>(contentType: "text/plain")
-            .Accepts<TokenGenerationRequest>(contentType: "application/json");
-
-        return app;
+            .Accepts<TokenGenerationRequest>(contentType: "application/json")
+            .Produces<string>(contentType: "application/jwt")
+            .WithOpenApi();
     }
 }
--- Helpers/Identity.Api/Program.cs
+++ Helpers/Identity.Api/Program.cs
@@ -1,27 +1,42 @@
 using Identity.Api.Endpoints;
+using Identity.Api.Logging;
 
 using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-var config = builder.Configuration;
+Log.Logger = IdentityApiLogging.CreateBootstrapLogger();
 
-Log.Logger = new LoggerConfiguration()
-    .ReadFrom.Configuration(config)
-    .CreateLogger();
-
-builder.Host.UseSerilog();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("Identity API application is starting...");
+
+    var builder = WebApplication.CreateBuilder(args);
+    {
+        builder.Host.UseSerilog((context, logConfig) =>
+            logConfig.ReadFrom.Configuration(context.Configuration));
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+    }
+
+    await using var app = builder.Build();
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseSerilogRequestLogging();
+        app.MapCreateToken();
+    }
+
+    app.Run();
 }
-
-app.MapCreateToken();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Identity API application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
